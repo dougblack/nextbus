6bf3d0523eaa832a12d42e66c79a423a18e07685
Doug Black <doug@itsdoug.com>
2012-03-01 14:40:51 -0500

    Changed a few icons to be slightly prettier.

--- gen/com/viewpagerindicator/R.java
+++ gen/com/viewpagerindicator/R.java
@@ -8,6 +8,10 @@
 package com.viewpagerindicator;
 
 public final class R {
+    public static final class anim {
+        public static final int push_right_in=0x7f040000;
+        public static final int push_right_out=0x7f040001;
+    }
     public static final class attr {
         /**  Whether or not the indicators should be centered. 
          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
@@ -243,57 +247,57 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int vpiTitlePageIndicatorStyle=0x7f010001;
     }
     public static final class bool {
-        public static final int default_circle_indicator_centered=0x7f080000;
-        public static final int default_circle_indicator_snap=0x7f080001;
-        public static final int default_title_indicator_selected_bold=0x7f080002;
+        public static final int default_circle_indicator_centered=0x7f090000;
+        public static final int default_circle_indicator_snap=0x7f090001;
+        public static final int default_title_indicator_selected_bold=0x7f090002;
     }
     public static final class color {
-        public static final int black=0x7f070011;
-        public static final int blue=0x7f07000e;
-        public static final int dark=0x7f070015;
-        public static final int darkpurple=0x7f070014;
-        public static final int default_circle_indicator_fill_color=0x7f070008;
-        public static final int default_circle_indicator_stroke_color=0x7f070009;
-        public static final int default_title_indicator_footer_color=0x7f07000a;
-        public static final int default_title_indicator_selected_color=0x7f07000b;
-        public static final int default_title_indicator_text_color=0x7f07000c;
-        public static final int fade1=0x7f07001b;
-        public static final int fade2=0x7f07001c;
-        public static final int fade3=0x7f07001d;
-        public static final int gray=0x7f070013;
-        public static final int green=0x7f07000f;
-        public static final int indicatorcolor=0x7f070018;
-        public static final int night=0x7f070019;
-        public static final int oldtitlecolor=0x7f070017;
-        public static final int orange=0x7f07001e;
-        public static final int red=0x7f07000d;
-        public static final int subtitlecolor=0x7f07001a;
-        public static final int themecolor=0x7f07001f;
-        public static final int titlecolor=0x7f070016;
-        public static final int vpi__background_holo_dark=0x7f070000;
-        public static final int vpi__background_holo_light=0x7f070001;
-        public static final int vpi__bright_foreground_disabled_holo_dark=0x7f070004;
-        public static final int vpi__bright_foreground_disabled_holo_light=0x7f070005;
-        public static final int vpi__bright_foreground_holo_dark=0x7f070002;
-        public static final int vpi__bright_foreground_holo_light=0x7f070003;
-        public static final int vpi__bright_foreground_inverse_holo_dark=0x7f070006;
-        public static final int vpi__bright_foreground_inverse_holo_light=0x7f070007;
-        public static final int vpi__dark_theme=0x7f070020;
-        public static final int vpi__light_theme=0x7f070021;
-        public static final int white=0x7f070012;
-        public static final int yellow=0x7f070010;
+        public static final int black=0x7f080011;
+        public static final int blue=0x7f08000e;
+        public static final int dark=0x7f080015;
+        public static final int darkpurple=0x7f080014;
+        public static final int default_circle_indicator_fill_color=0x7f080008;
+        public static final int default_circle_indicator_stroke_color=0x7f080009;
+        public static final int default_title_indicator_footer_color=0x7f08000a;
+        public static final int default_title_indicator_selected_color=0x7f08000b;
+        public static final int default_title_indicator_text_color=0x7f08000c;
+        public static final int fade1=0x7f08001b;
+        public static final int fade2=0x7f08001c;
+        public static final int fade3=0x7f08001d;
+        public static final int gray=0x7f080013;
+        public static final int green=0x7f08000f;
+        public static final int indicatorcolor=0x7f080018;
+        public static final int night=0x7f080019;
+        public static final int oldtitlecolor=0x7f080017;
+        public static final int orange=0x7f08001e;
+        public static final int red=0x7f08000d;
+        public static final int subtitlecolor=0x7f08001a;
+        public static final int themecolor=0x7f08001f;
+        public static final int titlecolor=0x7f080016;
+        public static final int vpi__background_holo_dark=0x7f080000;
+        public static final int vpi__background_holo_light=0x7f080001;
+        public static final int vpi__bright_foreground_disabled_holo_dark=0x7f080004;
+        public static final int vpi__bright_foreground_disabled_holo_light=0x7f080005;
+        public static final int vpi__bright_foreground_holo_dark=0x7f080002;
+        public static final int vpi__bright_foreground_holo_light=0x7f080003;
+        public static final int vpi__bright_foreground_inverse_holo_dark=0x7f080006;
+        public static final int vpi__bright_foreground_inverse_holo_light=0x7f080007;
+        public static final int vpi__dark_theme=0x7f080020;
+        public static final int vpi__light_theme=0x7f080021;
+        public static final int white=0x7f080012;
+        public static final int yellow=0x7f080010;
     }
     public static final class dimen {
-        public static final int default_circle_indicator_radius=0x7f0a0000;
-        public static final int default_circle_indicator_stroke_width=0x7f0a0001;
-        public static final int default_title_indicator_clip_padding=0x7f0a0002;
-        public static final int default_title_indicator_footer_indicator_height=0x7f0a0004;
-        public static final int default_title_indicator_footer_indicator_underline_padding=0x7f0a0005;
-        public static final int default_title_indicator_footer_line_height=0x7f0a0003;
-        public static final int default_title_indicator_footer_padding=0x7f0a0006;
-        public static final int default_title_indicator_text_size=0x7f0a0007;
-        public static final int default_title_indicator_title_padding=0x7f0a0008;
-        public static final int default_title_indicator_top_padding=0x7f0a0009;
+        public static final int default_circle_indicator_radius=0x7f0b0000;
+        public static final int default_circle_indicator_stroke_width=0x7f0b0001;
+        public static final int default_title_indicator_clip_padding=0x7f0b0002;
+        public static final int default_title_indicator_footer_indicator_height=0x7f0b0004;
+        public static final int default_title_indicator_footer_indicator_underline_padding=0x7f0b0005;
+        public static final int default_title_indicator_footer_line_height=0x7f0b0003;
+        public static final int default_title_indicator_footer_padding=0x7f0b0006;
+        public static final int default_title_indicator_text_size=0x7f0b0007;
+        public static final int default_title_indicator_title_padding=0x7f0b0008;
+        public static final int default_title_indicator_top_padding=0x7f0b0009;
     }
     public static final class drawable {
         public static final int blue_arrow=0x7f020000;
@@ -305,85 +309,86 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int green_dot=0x7f020006;
         public static final int greencell=0x7f020007;
         public static final int ic_bus_icon=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int ic_menu_compass=0x7f02000a;
-        public static final int ic_menu_home=0x7f02000b;
-        public static final int ic_menu_info_details=0x7f02000c;
-        public static final int ic_menu_mapmode=0x7f02000d;
-        public static final int ic_menu_mylocation=0x7f02000e;
-        public static final int ic_menu_preferences=0x7f02000f;
-        public static final int ic_menu_refresh=0x7f020010;
-        public static final int ic_menu_star=0x7f020011;
-        public static final int nightcell=0x7f020012;
-        public static final int purple_arrow=0x7f020013;
-        public static final int purple_dot=0x7f020014;
-        public static final int rate_star_big_off=0x7f020015;
-        public static final int red_arrow=0x7f020016;
-        public static final int red_dot=0x7f020017;
-        public static final int redcell=0x7f020018;
-        public static final int secondarrivalbg=0x7f020019;
-        public static final int sym_action_add=0x7f02001a;
-        public static final int titlebardrawable=0x7f02001b;
-        public static final int vpi__tab_indicator=0x7f02001c;
-        public static final int vpi__tab_selected_focused_holo=0x7f02001d;
-        public static final int vpi__tab_selected_holo=0x7f02001e;
-        public static final int vpi__tab_selected_pressed_holo=0x7f02001f;
-        public static final int vpi__tab_unselected_focused_holo=0x7f020020;
-        public static final int vpi__tab_unselected_holo=0x7f020021;
-        public static final int vpi__tab_unselected_pressed_holo=0x7f020022;
-        public static final int yellow_arrow=0x7f020023;
-        public static final int yellow_dot=0x7f020024;
-        public static final int yellowcell=0x7f020025;
+        public static final int ic_dialog_map=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int ic_menu_compass=0x7f02000b;
+        public static final int ic_menu_home=0x7f02000c;
+        public static final int ic_menu_info_details=0x7f02000d;
+        public static final int ic_menu_mapmode=0x7f02000e;
+        public static final int ic_menu_mylocation=0x7f02000f;
+        public static final int ic_menu_preferences=0x7f020010;
+        public static final int ic_menu_refresh=0x7f020011;
+        public static final int ic_menu_star=0x7f020012;
+        public static final int nightcell=0x7f020013;
+        public static final int purple_arrow=0x7f020014;
+        public static final int purple_dot=0x7f020015;
+        public static final int rate_star_big_off=0x7f020016;
+        public static final int red_arrow=0x7f020017;
+        public static final int red_dot=0x7f020018;
+        public static final int redcell=0x7f020019;
+        public static final int secondarrivalbg=0x7f02001a;
+        public static final int sym_action_add=0x7f02001b;
+        public static final int titlebardrawable=0x7f02001c;
+        public static final int vpi__tab_indicator=0x7f02001d;
+        public static final int vpi__tab_selected_focused_holo=0x7f02001e;
+        public static final int vpi__tab_selected_holo=0x7f02001f;
+        public static final int vpi__tab_selected_pressed_holo=0x7f020020;
+        public static final int vpi__tab_unselected_focused_holo=0x7f020021;
+        public static final int vpi__tab_unselected_holo=0x7f020022;
+        public static final int vpi__tab_unselected_pressed_holo=0x7f020023;
+        public static final int yellow_arrow=0x7f020024;
+        public static final int yellow_dot=0x7f020025;
+        public static final int yellowcell=0x7f020026;
     }
     public static final class id {
-        public static final int aboutmenusitem=0x7f06002b;
-        public static final int arrivalList=0x7f060025;
-        public static final int arrivalView=0x7f06001d;
-        public static final int arrivalsDrawer=0x7f060024;
-        public static final int blueButton=0x7f06000d;
-        public static final int colorBand=0x7f060028;
-        public static final int colorSeperator=0x7f060020;
-        public static final int colorbar=0x7f060018;
-        public static final int contactList=0x7f060006;
-        public static final int credits=0x7f060007;
-        public static final int directionTextView=0x7f060017;
-        public static final int drawerHandle=0x7f060026;
-        public static final int drawerTextView=0x7f060027;
-        public static final int firstArrival=0x7f06001e;
-        public static final int fourthArrival=0x7f060022;
-        public static final int frameLayout1=0x7f06001b;
-        public static final int gpsProgressBar=0x7f060010;
-        public static final int greenButton=0x7f06000e;
-        public static final int horizontal=0x7f060000;
-        public static final int linearLayout1=0x7f06000f;
-        public static final int mainlayout=0x7f060008;
-        public static final int mapButton=0x7f060012;
-        public static final int mapview=0x7f06000a;
-        public static final int nearestStopsButton=0x7f060014;
-        public static final int nearestStopsList=0x7f060011;
-        public static final int none=0x7f060002;
-        public static final int preferencesmenuitem=0x7f06002a;
-        public static final int redButton=0x7f06000b;
-        public static final int refreshButton=0x7f06001f;
-        public static final int routepagerviewpager=0x7f060016;
-        public static final int routes=0x7f060015;
-        public static final int routeviewprogressbar=0x7f06001c;
-        public static final int secondArrival=0x7f060023;
-        public static final int stopListView=0x7f06001a;
-        public static final int stopTextView=0x7f060019;
-        public static final int textView1=0x7f060013;
-        public static final int thirdArrival=0x7f060021;
-        public static final int titleBar=0x7f060005;
-        public static final int titleText=0x7f060009;
-        public static final int tohome=0x7f060029;
-        public static final int triangle=0x7f060003;
-        public static final int underline=0x7f060004;
-        public static final int vertical=0x7f060001;
-        public static final int yellowButton=0x7f06000c;
+        public static final int aboutmenusitem=0x7f07002b;
+        public static final int arrivalList=0x7f070025;
+        public static final int arrivalView=0x7f07001d;
+        public static final int arrivalsDrawer=0x7f070024;
+        public static final int blueButton=0x7f07000d;
+        public static final int colorBand=0x7f070028;
+        public static final int colorSeperator=0x7f070020;
+        public static final int colorbar=0x7f070018;
+        public static final int contactList=0x7f070006;
+        public static final int credits=0x7f070007;
+        public static final int directionTextView=0x7f070017;
+        public static final int drawerHandle=0x7f070026;
+        public static final int drawerTextView=0x7f070027;
+        public static final int firstArrival=0x7f07001e;
+        public static final int fourthArrival=0x7f070022;
+        public static final int frameLayout1=0x7f07001b;
+        public static final int gpsProgressBar=0x7f070010;
+        public static final int greenButton=0x7f07000e;
+        public static final int horizontal=0x7f070000;
+        public static final int linearLayout1=0x7f07000f;
+        public static final int mainlayout=0x7f070008;
+        public static final int mapButton=0x7f070012;
+        public static final int mapview=0x7f07000a;
+        public static final int nearestStopsButton=0x7f070014;
+        public static final int nearestStopsList=0x7f070011;
+        public static final int none=0x7f070002;
+        public static final int preferencesmenuitem=0x7f07002a;
+        public static final int redButton=0x7f07000b;
+        public static final int refreshButton=0x7f07001f;
+        public static final int routepagerviewpager=0x7f070016;
+        public static final int routes=0x7f070015;
+        public static final int routeviewprogressbar=0x7f07001c;
+        public static final int secondArrival=0x7f070023;
+        public static final int stopListView=0x7f07001a;
+        public static final int stopTextView=0x7f070019;
+        public static final int textView1=0x7f070013;
+        public static final int thirdArrival=0x7f070021;
+        public static final int titleBar=0x7f070005;
+        public static final int titleText=0x7f070009;
+        public static final int tohome=0x7f070029;
+        public static final int triangle=0x7f070003;
+        public static final int underline=0x7f070004;
+        public static final int vertical=0x7f070001;
+        public static final int yellowButton=0x7f07000c;
     }
     public static final class integer {
-        public static final int default_circle_indicator_orientation=0x7f090000;
-        public static final int default_title_indicator_footer_indicator_style=0x7f090001;
+        public static final int default_circle_indicator_orientation=0x7f0a0000;
+        public static final int default_title_indicator_footer_indicator_style=0x7f0a0001;
     }
     public static final class layout {
         public static final int credits=0x7f030000;
@@ -396,28 +401,28 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int vpi__tab=0x7f030007;
     }
     public static final class menu {
-        public static final int about_menu=0x7f0d0000;
-        public static final int stock_menu=0x7f0d0001;
+        public static final int about_menu=0x7f0e0000;
+        public static final int stock_menu=0x7f0e0001;
     }
     public static final class raw {
-        public static final int routeconfig=0x7f050000;
+        public static final int routeconfig=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f0c0001;
-        public static final int hello=0x7f0c0000;
+        public static final int app_name=0x7f0d0001;
+        public static final int hello=0x7f0d0000;
     }
     public static final class style {
-        public static final int TextAppearance_TabPageIndicator=0x7f0b0006;
-        public static final int Theme_DarkTheme=0x7f0b0007;
-        public static final int Theme_PageIndicatorDefaults=0x7f0b0000;
-        public static final int Widget=0x7f0b0001;
-        public static final int Widget_CirclePageIndicator=0x7f0b0002;
-        public static final int Widget_TabPageIndicator=0x7f0b0004;
-        public static final int Widget_TabPageIndicator_Text=0x7f0b0005;
-        public static final int Widget_TitlePageIndicator=0x7f0b0003;
+        public static final int TextAppearance_TabPageIndicator=0x7f0c0006;
+        public static final int Theme_DarkTheme=0x7f0c0007;
+        public static final int Theme_PageIndicatorDefaults=0x7f0c0000;
+        public static final int Widget=0x7f0c0001;
+        public static final int Widget_CirclePageIndicator=0x7f0c0002;
+        public static final int Widget_TabPageIndicator=0x7f0c0004;
+        public static final int Widget_TabPageIndicator_Text=0x7f0c0005;
+        public static final int Widget_TitlePageIndicator=0x7f0c0003;
     }
     public static final class xml {
-        public static final int preferences=0x7f040000;
+        public static final int preferences=0x7f050000;
     }
     public static final class styleable {
         /** Attributes that can be used with a CirclePageIndicator.
